a492a17600f3ed5ff91f47537224759b0849425a
citrus1998 <61907831+citrus1998@users.noreply.github.com>
2022-12-15 12:24:48 +0900

    Update 2darray.scm

--- templete/2darray.scm
+++ templete/2darray.scm
@@ -24,19 +24,14 @@
     )
   )
 )
-#;(vector-set! V i (vector-of-length-ec m [:port idx (current-input-port)] idx))
+
 (define (2darray V i m)
   (let ((W (make-vector m)))
     (if (= i (vector-length V))
       V
       (begin
         (1darray W 0)
-        #;(print W)
-        #;(newline)
         (vector-set! V i W)
-        #;(print V)
-        #;(newline)
-        #;(newline)
         #;(newline)
         (2darray V (+ i 1) m)
       )
@@ -48,8 +43,7 @@
       [M (read)])
   (begin 
     (define A (make-vector N))
-    #;(print A)
     (2darray A 0 M)
-    (pretty-print A)
+    (print A)
   )
 )
