634138dd39312534a2e89f9659911aa64d9e54f5
s528661 <61907831+s528661@users.noreply.github.com>
2023-04-01 22:44:05 +0900

    Create A.cpp

---
+++ 296/A.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <stack>
+#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+using std::cout; using std::vector;
+using std::array; using std::rotate;
+using std::endl; using std::sort;
+
+#define rep0(i,n) for (int i = 0; i < (int)(n); i++)
+#define rep(i,n) for (int i = 1; i <= (int)(n); i++)
+#define INFTY 1000000001
+
+template<typename T>
+void printElements(T &v)
+{
+    //cout << "[ ";
+    for (const auto &item : v) {
+        //cout << item << ", ";
+        cout << item << " ";
+    }
+    //cout << "\b\b ]" << endl
+    cout << "" << endl;
+}
+
+int isTr(vector<int>& nms, int nm){
+  if (std::find(nms.begin(), nms.end(), nm) != nms.end()) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+int vector_finder(std::vector<int> vec, int number) {
+  auto itr = std::find(vec.begin(), vec.end(), number);
+  size_t index = std::distance( vec.begin(), itr );
+  if (index != vec.size()) { // 発見できたとき
+    return 1;
+  }
+  else { // 発見できなかったとき
+    return 0;
+  }
+}
+
+int main() 
+{
+    int i,j,k;
+    int N,M;
+    cin >> N;
+    
+    string S;
+    cin >> S;
+    
+    bool flag = true;
+    
+    rep0(i,N){
+      if(S[i]=='F'){
+        if(S[i+1]=='F') {flag = false; break;}
+        else if(N==i+1) {break;}
+      }else if(S[i]=='M'){
+        if(S[i+1]=='M') {flag = false; break;}
+        else if(N==i+1) {break;}
+      }
+    }
+    
+    if(flag) cout << "Yes";
+    else cout << "No";
+    cout << endl;
+  
+    return 0;
+}
